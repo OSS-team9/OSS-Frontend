29e9507080da5f20be975187bf05bb0ea0c0fd36
Jeongin205 <ji801089@gmail.com>
2025-12-14 03:13:02 +0900

    fix:바텀네비 z수정

--- src/components/layout/BottomNav.tsx
+++ src/components/layout/BottomNav.tsx
@@ -18,7 +18,7 @@ export default function BottomNav() {
     "flex items-center justify-center w-10 h-10 text-app-bg";
   const iconStyle = "w-7 h-7";
   return (
-    <nav className="fixed bottom-0 left-0 right-0 h-16 bg-app-bg-tertiary">
+    <nav className="fixed bottom-0 left-0 right-0 h-16 bg-app-bg-tertiary z-100">
       <div className="flex justify-around items-center h-full max-w-md mx-auto">
         {/* 'Link' 태그로 각 페이지를 연결합니다. */}
         <Link href="/main" className={iconWrapperStyle}>
