3cefe3720aedaf2be977279fc28cd3de09e9eb3c
Jeongin205 <ji801089@gmail.com>
2025-12-16 03:26:55 +0900

    Revert "design: 로그인 버튼"
    This reverts commit d2ae1354fcf3b1ef6aa9fb669d41d2353e12144b.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function LandingPage() {
 
       <div className="w-full max-w-sm bg-white/80 backdrop-blur-sm px-4 py-8 rounded-[2.5rem] shadow-xl border-4 border-white">
         <div className="flex flex-col gap-4">
-          <div className="flex justify-center w-full shrink-0">
+          <div className="flex justify-center w-full">
             <GoogleLogin
               onSuccess={handleLoginSuccess}
               onError={() => alert("로그인 실패")}
